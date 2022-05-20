adcff22cc51dc47d4cadb97754fb721ef8aef215
JavierPolaris <javierrojocanton@gmail.com>
2022-05-20 19:04:12 +0200

    conseguimos la grafica que funcione y recoja los datos

--- HojaEnBlanco/codigo.js
+++ HojaEnBlanco/codigo.js
@@ -1,88 +1,100 @@
-// Utilizando la siguiente API: https://dog.ceo/dog-api/
-
-// Que devuelve imágenes aleatorias, habrá que ir coleccionando diferentes razas de perros pulsando un botón e ir rellenando una matriz (4x5) como la que se ve en las imágenes.
-
-// Se va añadiendo cada imágen a una casilla, además de almacenar los perros que van apareciendo para poder sacar estadísticas al final.
-
-// En caso de que alguna raza esté repetida, no se pondrá en la colección.
-
-// Cuando se haya completado todo el álbum aparecerá una gráfica de todas las razas que han salido y la de veces que se han ido contando.
-
-// Las imágenes ilustran el funcionamiento, pero la apariencia de la aplicación depende del grupo y se debe cuidar tanto como el funcionamiento; además se deben usar conocimientos de Flexbox y Grid en la medida de las posibilidades del diseño.
+//TODO Necesitamos que cuando la raza se repita no pinte el src de la galeria.
 
 
 
-// // TODO  razas
-// fetch('https://dog.ceo/api/breeds/list/all')             //BORRAMOS AL NO UTILIZAR ESTE FETCH??
-//     .then(res=>res.json())
-//     .then(json=>console.log(json));
-
-// a user is redirected to a different page with the location.replace method
-let razas = []
+let razas = [];
 let cont = 0;
-let contStorage = 1;
-//TODO boton random y almacenar la url de la imagen
-document.getElementById('btn').addEventListener('click', (e) => {
+
+document.getElementById('btn').addEventListener('click', (e) =>{
     e.preventDefault();
 
-
     fetch('https://dog.ceo/api/breeds/image/random')
-        .then(res => res.json())                                        //recoger los datos de la API
-        .then(json => crearImg(json));
+    .then(res => res.json())
+    .then(json => crearImg(json));
 
 
     function crearImg(url) {
-        document.getElementById('perro' + cont).src = `${url.message}`
+
+        //document.getElementById('perro' + cont).src = `${url.message}`
         document.getElementById('perro').src = `${url.message}`
 
-        let urlCortada = (url.message.split('/'));
+
+        let urlCortada = (url.message.split("/"));
         razas.push(urlCortada[4]);
 
-        let exi = localStorage.length;
-        // console.log(exi+'valor')
+        let puseoCon = [];
+        
+        let puseo = [];
+        let repetido = false;
+        let key;
+        for (let i = 0; i < localStorage.length; i++) {
+            key = localStorage.key(i);
+            puseo.push(localStorage.key(i));
+            
+            if (key == urlCortada[4]) {
+                
+                console.log('Repetido')
+                let contador = parseInt(JSON.parse(localStorage.getItem(`${key}`)).cazado)
+                contador++;
+                
 
-        if (exi == 0) {
-            localStorage.setItem(`perro${cont}`, JSON.stringify({
-                raza: `${urlCortada[4]}`,
-                contador: `${contStorage}`
-            }))
+                localStorage.setItem(urlCortada[4],JSON.stringify({
+                    cazado: contador
+                }));
 
-
-        } else {
-            for (let i = 0; i < localStorage.length; i++) {
-                let key = localStorage.key(i);
-
-                if (`${JSON.stringify(localStorage.getItem(key)).raza}` == urlCortada[4]) {
-
-                    console.log("REPEEEE")
-                    contStorage++;
-                } else {
-                    console.log('probar el else')
-                    localStorage.setItem(`perro${cont}`, JSON.stringify({
-                        raza: `${urlCortada[4]}`,
-                        contador: `${contStorage}`
-                    }))
-                }
+                cont--
+                repetido = true;
             }
+            puseoCon.push(JSON.parse(localStorage.getItem(`${key}`)).cazado)
+
+        }
+
+        if (!repetido) {
+           document.getElementById('perro' + cont).src = `${url.message}`
+
+            localStorage.setItem(urlCortada[4],JSON.stringify({
+                cazado: 1   
+            }));
+        }
+        
+console.log(puseo)
+// console.log(puseoCon)
+
+        let i=localStorage.length;
+        // let j = key
+        // var arr = Object.entries(j)
+        // console.log(arr)
+        // let puseoDeMierda = puseoCon.split('"cazado":');
+        // console.log(puseoDeMierda)
+
+        if(i>=20){
+            var array = puseo;
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: array,
+                    datasets: [{
+                        data: puseoCon,
+                        backgroundColor: [
+                            'rgba(255, 99, 132)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132)'
+                        ],
+                        borderWidth: 3
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
         }
     }
+cont++
 
-    // for(let i=0; i<localStorage.length; i++) {
-
-    //     let key = localStorage.key(i);
-    //     if (`${JSON.parse(localStorage.getItem(key)).raza}` == urlCortada[4]) {
-    //         console.log(key)
-    //         contStorage++;
-    //     }else {
-    //         localStorage.getItem(`perro${cont}`,JSON.stringify({
-    //             raza: `${urlCortada[4]}`,
-    //             contador: `${contStorage}`
-    //         }))
-    //     }
-    // }
-
-
-    cont++
-   
 });
-
